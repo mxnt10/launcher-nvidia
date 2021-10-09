f858d5c945beef2d73d9dc515fe930e2ac80347a
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-10-09 12:59:09 -0300

    Upload Files - 2021/10/09

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # launcher-nvidia
+
+Program created with the objective of assisting in the execution of Games and programs in Notebooks with Nvidia + Intel in Distributions such as Slackware.
+<br><br>
+
+
+## Install
+
+To install "Launcher Nvidia" , use the following commands:
+```sh
+$ git clone https://github.com/mxnt10/launcher-nvidia.git
+$ cd launcher-nvidia
+
+$ sudo ./install.sh
+```
--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 install_root=${install_root:-""}
 
-[ $install_root != "" ] && mkdir -p $install_root/usr/{bin,share/{applications,pixmaps,l-nvidia/tools}} || mkdir -p /usr/share/l-nvidia/tools
+[ $install_root != "" ] && mkdir -p $install_root/usr/{bin,share/{applications,pixmaps,l-nvidia/tools},doc/l-nvidia} || mkdir -p /usr/{share/l-nvidia/tools,doc/l-nvidia}
 
 install -Dm 0644 images/l-nvidia.png $install_root/usr/share/pixmaps
 install -Dm 0644 images/l-nvidia.desktop $install_root/usr/share/applications
 install -Dm 0644 src/*.py $install_root/usr/share/l-nvidia
 install -Dm 0755 tools/* $install_root/usr/share/l-nvidia/tools
+
+cp -a ChangeLog LICENSE README.md RELEASE $install_root/usr/doc/l-nvidia
 
 echo "#!/bin/bash
 cd /usr/share/l-nvidia
